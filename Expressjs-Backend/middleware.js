329352759771a9fcce83dcbe829d00f7cdf1c81d
KryptoKomal <kryp70girl@gmail.com>
2026-02-10 15:37:55 +0530

    10-feb classroom learning

--- Expressjs-Backend/middleware.js
+++ Expressjs-Backend/middleware.js
@@ -1,51 +1,51 @@
-// Middleware
 const fs = require("fs").promises;
 const express = require("express");
 const app = express();
 
-app.use(express.json());
+app.use(express.json())
 
-const PORT = 8000;
-
-// Middleware 1
-app.use((req, res, next) => {
-  console.log("I am middleware 1");
-  next();
+const PORT= 8000;
+app.listen(PORT, () => {
+  console.log("Server is listening on port:8000");
 });
 
-// Middleware 2
-app.use((req, res, next) => {
-  console.log("I am middleware 2");
-  next();
-});
-app.use((req , res,next)=>{
-  console.log=`${new Date(). toLocaleString()}`
-  fs.appendFile("log.txt",log, (err)=>{
-
-  })
+app.use((req,res,next) => {
+  console.log("I am middleware 1.")
+  next(); // yeh middleware ko call krega.
 })
 
-const fileMiddleware=(req,res,next)=>{
-  console.log("I am checking file access");
-  return res.send("auth file");
+// Creating custom middleware
+app.use((req,res,next) => {
+  console.log("I am middleware 2.")
+  next(); 
+})
+
+const logMiddleware=(req,res,next)=>{
+    const start=Date.now();
+    res.on("finish", async () => {
+    const duration = Date.now() - start;
+
+    const log = `${new Date().toISOString()} | ${req.method} ${req.originalUrl} | ${res.statusCode} | ${duration}ms\n`;
+
+    try {
+      await fs.appendFile("log.txt", log); // creates file if not present
+    } catch (err) {
+      console.error("Error writing log file:", err.message);
+    }
+  });
+  next();
+}
+app.use(logMiddleware);
+
+
+const fileAuthMiddleware = (req,res,next) => {
+  const token = req.headers["authorization"]
+  if(token !== "secrettoken") {
+    res.status(401).send("Invalid Token")
+  }
+  next();
 }
 
-app.use((req,res,next)=>{
-  const token = req.header["authorization"];
-});
-
-const authMiddleware =(req,res,next)=>{
-  const token = req.header["authorization"];
-  if(!token) return res.status().send ("please provide Auth token");
-  if(token =="secrettoken"){
-    next();
-  }
-  else{
-    return res.status(400).send("UnAuthorized");
-  }
-}
-
-// File functions
 const readStudentsFromFile = async () => {
   const data = await fs.readFile("./students.json", "utf-8");
   return JSON.parse(data || "[]");
@@ -55,13 +55,7 @@ const writeStudentsToFile = async (records) => {
   await fs.writeFile("./students.json", JSON.stringify(records, null, 2));
 };
 
-// Route
-app.get("/students",fileMiddleware, async (req, res) => {
-  const students = await readStudentsFromFile();
-  return res.status(200).json(students);
-});
-
-// Server start (only once)
-app.listen(PORT, () => {
-  console.log("Server is listening on port: 8000");
-});
+app.get("/students",fileAuthMiddleware,async(req, res) => {
+    const students= await readStudentsFromFile();
+    return res.status(200).json(students);
+})
